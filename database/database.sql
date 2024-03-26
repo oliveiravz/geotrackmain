004a67e66b20d4cf574e520630157d7f9e6066af
João Victor <49462692+oliveiravz@users.noreply.github.com>
2024-03-25 22:09:37 -0300

    Ajuste sintaxe

--- database/database.sql
+++ database/database.sql
@@ -6,12 +6,12 @@ CREATE TABLE coord(
     id VARCHAR(36) NOT NULL,
     latitude VARCHAR(8),
     longitude VARCHAR(8)
-    PRIMARY KEY id
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE dispositivo(
     id VARCHAR(36) NOT NULL,
     codigo VARCHAR(10),
     descricao VARCHAR(50)
-    PRIMARY KEY id
+    PRIMARY KEY (id)
 );
