266c17dab763643af9cb348d9ef11d736e28ec31
Pedro Gottlob <pedrogottlob@gmail.com>
2024-04-13 10:00:25 -0300

    broker

--- database/database.sql
+++ database/database.sql
@@ -5,13 +5,13 @@ USE geotrack;
 CREATE TABLE coord(
     id VARCHAR(36) NOT NULL,
     latitude VARCHAR(8),
-    longitude VARCHAR(8)
+    longitude VARCHAR(8),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE dispositivo(
     id VARCHAR(36) NOT NULL,
     codigo VARCHAR(10),
-    descricao VARCHAR(50)
+    descricao VARCHAR(50),
     PRIMARY KEY (id)
 );
