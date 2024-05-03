ffc3371e372909525d2af7534bfd831b6cab235f
João Victor <49462692+oliveiravz@users.noreply.github.com>
2024-05-02 23:33:11 -0300

    coordenadas

--- mqtt/publisher.php
+++ mqtt/publisher.php
@@ -12,9 +12,13 @@ $client_id = 'phpMQTT-publisher';
 
 $mqtt = new phpMQTT($server, $port, $client_id);
 
+$latlon = [ -22.209539, -49.948460 ];
+
+$json = json_encode($latlon);
+
 while (true) {
     if ($mqtt->connect(true, NULL, $username, $password)) {
-        $mqtt->publish("bluerhinos/phpMQTT/examples/publishtest", "Mensagem enviada em: " . date("d/m/Y H:i:s"), 0, false);
+        $mqtt->publish("bluerhinos/phpMQTT/examples/publishtest", "Coordenadas {$json} enviadas em: " . date("d/m/Y H:i:s"), 0, false);
         $mqtt->close();
     } else {
         echo "Time out!\n";
