8334d56a1115bb98a2a93282afba804587c1c3b7
João Victor <49462692+oliveiravz@users.noreply.github.com>
2024-05-21 22:24:30 -0300

    Enviando dados para a API de dados

--- mqtt/publisher.php
+++ mqtt/publisher.php
@@ -12,17 +12,57 @@ $client_id = 'phpMQTT-publisher';
 
 $mqtt = new phpMQTT($server, $port, $client_id);
 
-$latlon = [ -22.209539, -49.948460 ];
+$latlon = generateRandomCoordinates();
 
 $json = json_encode($latlon);
 
 while (true) {
     if ($mqtt->connect(true, NULL, $username, $password)) {
-        $mqtt->publish("geotrack/mqtt/cordinates", "Coordenadas - {$json} enviadas em: " . date("d/m/Y H:i:s"), 0, false);
+        
+        $mqtt->publish("geotrack/mqtt/coordinates", "Coordenadas - {$json} enviadas em: " . date("d/m/Y H:i:s"), 0, false);
+
+        $curl = curl_init();
+    
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'http://localhost:8081/geotrack/mqtt/coordinates',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $json,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json'
+            ),
+        ));
+    
+        $response = curl_exec($curl);
+    
+        curl_close($curl);
+        echo $response;
+
+
         $mqtt->close();
     } else {
         echo "Time out!\n";
     }
 
-    sleep(30);
+    sleep(5);
+}
+
+
+function generateRandomCoordinates() {
+    $coordinates = [];
+
+    $latitude = rand(-9000000, 9000000) / 100000;
+    $longitude = rand(-18000000, 18000000) / 100000;
+
+    $coordinates[] = [
+        'latitude' => $latitude,
+        'longitude' => $longitude
+    ];
+
+    return $coordinates;
 }
--- mqtt/subscriber.php
+++ mqtt/subscriber.php
@@ -15,10 +15,8 @@ if($mqtt->connect(true, NULL, $username, $password)) {
 	
     $mqtt->debug = true;
     
-    $topics['geotrack/mqtt/cordinates'] = array('qos' => 0, 'function' => 'messageReceive');
+    $topics['geotrack/mqtt/coordinates'] = array('qos' => 0, 'function' => 'messageReceive');
     $mqtt->subscribe($topics, 0);
-    
-    sendToApi($topics);
 
     while($mqtt->proc()) {
     
@@ -30,35 +28,8 @@ if($mqtt->connect(true, NULL, $username, $password)) {
     exit(1);
 }
 
-
 function messageReceive($topic, $msg){
     echo 'Mensagem recebida: ' . date("d/m/Y H:i:s") . "\n";
     echo "Tópico: {$topic}\n\n";
     echo "\t$msg\n\n";
-}
-
-function sendToApi($data) {
-
-    $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => 'http://localhost:8080/geotrack/mqtt/cordinates',
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_POSTFIELDS => json_encode($data),
-        CURLOPT_HTTPHEADER => array(
-            'Content-Type: application/json'
-        ),
-    ));
-
-    $response = curl_exec($curl);
-
-    curl_close($curl);
-    echo $response; 
-    
 }
